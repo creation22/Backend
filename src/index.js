eefa9d4955eec1bd73d44ebed16e697395ed3bd6
creation <creation2224@gmail.com>
2025-04-21 15:49:15 +0530

    custom api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require('dotenv').config({path : './env'})
 import dotenv from  "dotenv"
-
+import {app} from './app.js'
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -8,6 +8,20 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+            app.on("error" , (error) => {
+            console.log("Err" , error);
+            throw error
+        })
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`server is running at port ${process.env.PORT}`);
+        
+    } )
+})
+.catch((err) => {
+    console.log("mongodb connection failed" ,err);
+    
+})
 
 
 
